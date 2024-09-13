620355a94a6484f9b3d216426ca386e27cedf547
kolotskiI <ishielmishm24@gmail.com>
2024-09-13 21:57:44 +0800

    Inital commit

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNum = 24;
+let secondFavNum= 19;
+
+//greater than
+console.log(firstFavNum > secondFavNum);
+
+//less than
+console.log(firstFavNum < secondFavNum); 
+
+//greater than or equal to
+console.log(firstFavNum >= secondFavNum); 
+
+//less than to equal to
+console.log(firstFavNum <= secondFavNum); 
+
+//strict equality (===)
+console.log(firstFavNum === secondFavNum); 
+
+// loose equality  (==)
+console.log(firstFavNum == secondFavNum);
+
+//strict inequality (!==)
+console.log(firstFavNum !== secondFavNum);
+
+//loose inequality (!=)
+console.log(firstFavNum != secondFavNum);
+
+//AND operator
+console.log(firstFavNum > 24 && secondFavNum < 19);
+
+//OR operator
+console.log(firstFavNum < 24 || secondFavNum > 19);
+
+//NOT operator
+console.log(!(firstFavNum > secondFavNum))
